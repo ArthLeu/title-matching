ea040ce012cd81f508da10646ae4bb26934a804d
ArthLeu <haoyanl2@uci.edu>
2021-04-29 20:28:04 +0800

    added basic LCS algorithm

--- LCS.py
+++ LCS.py
@@ -1,11 +1,37 @@
 import time
+import numpy as np
 
 
 def LongestCommonSubstring(document, phrase):
-    ''' input: document, phrase to match; output: longest matching substring '''
-    pass
+    ''' input: document (str), phrase to match (str); output: longest matching substring '''
+    m = len(document) + 1 # add 1 because the first row/col is for when substring length is 0
+    n = len(phrase) + 1
+    T = np.zeros([m, n])
+    maxlen = 0
+    argmaxstr = ""
+
+    for i in range(1, m):
+        for j in range(1, n):
+            if document[i-1] == phrase[j-1]: # subtract 1 because the index of doc/phrase is still 0-based
+                T[i, j] = T[i-1, j-1] + 1
+            else:
+                T[i, j] = 0
+
+            # update record
+            if T[i, j] > maxlen:
+                maxlen = int(T[i, j])
+                argmaxstr = phrase[j-maxlen:j]
+
+    # finalize results
+    print("longest substring:", argmaxstr)
+    print("length:", maxlen)
+    
+    return (argmaxstr, maxlen)
 
 
 if __name__ == "__main__":
     localtime = time.asctime(time.localtime(time.time()))
     print(localtime)
+
+    LongestCommonSubstring("abcdefg", "cdea")
+    
