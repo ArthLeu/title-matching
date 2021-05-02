6062bdbea643d31d39a2b2040fb40f500f8f3d94
ArthLeu <haoyanl2@uci.edu>
2021-05-02 15:03:28 +0800

    added packages

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
+import os
+import numpy
+import matplotlib.pyplot as plt
+
 import LCS
+
 
 
 def main():
     ''' this is the main function '''
     
     # PART 01: preprocess the documents
-
+    
 
     # PART 02: run matching algorithms in each document
-
+    
 
     # PART 03: output statistics
 
